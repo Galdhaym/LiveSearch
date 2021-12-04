9fa8588aed19a6a0827fccb9df3aa7c5a3dc6bd3
UnnamedH <55529629+UnnamedH@users.noreply.github.com>
2021-12-04 12:06:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+var cities = ["Одесса", "Киев", "Житомир", "Лондон", "Нью-Йорк", "Львов", "Черновцы", "Берлин", "Москва"];
+
+function getOccurrenceOfArrayByString(strings, currentString) {
+	let arrayOfOccurences = [];
+	if (currentString === "") {
+		return arrayOfOccurences;
+	}
+	for (let string of strings) {
+		if (string.indexOf(currentString) > -1) {
+			arrayOfOccurences.push(string);
+		}
+	}
+	return arrayOfOccurences;
+}
+
+function fillSearchListByArray(array) {
+	clearSearchList();
+	var list = document.querySelector(".search__list");
+	if (array.length === 0) {
+		var li = document.createElement("li");
+		li.innerHTML = "Not Found";
+		list.appendChild(li);
+		return;
+	}
+	for (let element of array) {
+		var li = document.createElement("li");
+		li.innerHTML = element;
+		list.addEventListener("click", function (e) {
+			var liElement = e.target;
+			if (liElement.tagName === "LI") {
+				var input = document.querySelector(".liveSearch input");
+				input.value = liElement.innerHTML;
+			}
+		});
+		list.appendChild(li);
+	}
+	list.classList.add("open");
+}
+
+function clearSearchList() {
+	var list = document.querySelector(".search__list");
+	while (list.firstChild) {
+		list.removeChild(list.firstChild);
+	}
+}
+
+var liveSearchInput = document.querySelector(".liveSearch input");
+liveSearchInput.addEventListener("input", function (e) {
+	var inputValue = e.target.value;
+	let arrayOfOccurences = getOccurrenceOfArrayByString(cities, inputValue);
+	fillSearchListByArray(arrayOfOccurences);
+});
